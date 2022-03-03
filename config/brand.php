cd9d08f9f5088aae68cf60585a907e89e33a0ee1
awcodes <adam@adamwgd.com>
2022-03-03 09:22:55 -0500

    feat: add brand config

---
+++ config/brand.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+    'name' => config('app.name', ''),
+    'display' => '',
+    'source' => '',
+    'slug' => '',
+    'code' => '',
+    'phone' => '',
+    'vanity' => '',
+    'email' => '',
+    'street' => '',
+    'unit' => '',
+    'city' => '',
+    'state' => '',
+    'zip' => '',
+    'website' => config('app.url', ''),
+    'ola_token' => '',
+    'images' => [
+        'logo' => [
+            'url' => '',
+            'alt' => '',
+            'width' => '',
+            'height' => '',
+        ],
+        'logo_reversed' => [
+            'url' => '',
+            'alt' => '',
+            'width' => '',
+            'height' => '',
+        ],
+        'icon' => [
+            'url' => '',
+            'alt' => '',
+            'width' => '',
+            'height' => '',
+        ],
+        'opengraph' => [
+            'url' => '',
+            'alt' => '',
+            'width' => '',
+            'height' => '',
+        ],
+    ],
+    'additional_images' => [],
+    ],
+    'apps' => [
+        'portal' => env('CUSTOMER_PORTAL_URL'),
+        'ecommerce' => env('ECOMMERCE_APP_URL'),
+        'android' => env('ANDROID_APP_URL'),
+        'ios' => env('IOS_APP_URL'),
+    ],
+    'social_media' => [
+        'facebook' => null,
+        'twitter' => null,
+        'instagram' => null,
+        'pinterest' => null,
+        'linkedin' => null,
+        'youtube' => null,
+        'glassdoor' => null,
+    ],
+];
